dc1140cc0ff6086b9310c441ea6e93be269f9582
shiftgithub <mhturanmohammad@gmail.com>
2023-06-02 15:28:16 +0600

    fixed : add fom.html

--- api/urls.py
+++ api/urls.py
@@ -40,36 +40,60 @@ urlpatterns = [
 
     path('', web.Dashboard, name="dashboard"),
 
+    # department frontend path
+
     path('frontend/add/department/', web.DepartmentForm,
          name="add_department_form"),
+    path('frontend/store/department/', web.StoreDepartment, name="store_department"),
     path('frontend/department/list/',
          web.DepartmentDataView, name="department_list"),
+     
+     # department specification frontend path
+
+    path('frontend/add/department/specification/', web.DepartmentSpecificationForm,
+         name="add_department_specification_form"),
+     path('frontend/store/department/specification/', web.StoreDepartmentSpecification, name="store_department_specification"),
+    path('frontend/department/specification/list/',
+         web.DepartmentSpecificationDataView, name="department_specification_list"),
+
+    # doctor frontend path
 
     path('frontend/add/doctor/', web.DoctorForm, name="add_doctor_form"),
+    path('frontend/store/doctor/', web.StoreDoctor, name="store_doctor"),
     path('frontend/doctors/list/', web.DoctorDataView, name="doctor_list"),
+
+    # bodypart frontend path
 
     path('frontend/add/bodypart/', web.BodyPartForm, name="add_bodypart_form"),
     path('frontend/store/bodypart/', web.StoreBodyPart, name="store_bodypart"),
     path('frontend/bodypart/list/', web.BodyPartDataView, name="bodypart_list"),
 
+    # organ frontend path
+
     path('frontend/add/organ/', web.OrganForm, name="add_organ_form"),
+    path('frontend/store/organ/', web.StoreOrgan, name="store_organ"),
     path('frontend/organ/list/', web.OrganDataView, name="organ_list"),
 
-    path('frontend/add/organ/problem/', web.OrganProblemForm,
+    # organ problem frontend path
+
+     path('frontend/add/organ/problem/', web.OrganProblemForm,
          name="add_organ_problem_form"),
-    path('frontend/organ/problem/list/',
+     path('frontend/store/organ/problem/', web.StoreOrganProblem, name="store_organ_problem"),
+     path('frontend/organ/problem/list/',
          web.OrganProblemDataView, name="organ_problem_list"),
+
+    # problem specification frontend path
 
     path('frontend/add/problem/specification/', web.ProblemSpecificationForm,
          name="add_problem_specification_form"),
+    path('frontend/store/problem/specification/', web.StoreProblemSpecification, name="store_problem_specification"),
     path('frontend/problem/specification/list/',
          web.ProblemSpecificationDataView, name="problem_specification_list"),
 
-    path('frontend/add/department/specification/', web.DepartmentSpecificationForm,
-         name="add_department_specification_form"),
-    path('frontend/department/specification/list/',
-         web.DepartmentSpecificationDataView, name="department_specification_list"),
+    
+    # dashboard frontend path
 
      path('myadmin/dashboard/', web.GetAdminDashBoard, name="admin_dashboard"),
+
      path('myadmin/form/', web.GetAdminForm, name="admin_form"),
 ]
