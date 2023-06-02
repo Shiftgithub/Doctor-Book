7eac159956c6e7f4b5536bc36a68ee32a52ed443
robinNcode <msmrobin518@gmail.com>
2023-06-02 18:11:12 +0600

    upd: UI table and btn small class added
    Signed-off-by: robinNcode <msmrobin518@gmail.com>

--- static/adminlte_assets/js/activeSideBar.js
+++ static/adminlte_assets/js/activeSideBar.js
@@ -1,5 +1,6 @@
 let allElements = $('.nav-item.mainItem');
 const url = window.location.href;
+
 $.each(allElements, function (index, element) {
 // getting all the children of parent li
 let module = $(element).children();
@@ -15,8 +16,9 @@ let module = $(element).children();
         $.each(sections, function (index, section) {
             // li of selected ul
             let li = $(section).children();
-            // li.attr('href') will return the clicked
-            if (url === li.attr('href')) {
+            let liHref = 'http://127.0.0.1:8000' + li.attr('href');
+
+            if (url === liHref) {
                 li.addClass('active');
                 li.parent().parent().parent().addClass('menu-open');
                 $(module[0]).addClass('active');
