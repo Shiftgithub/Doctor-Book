c8760d4075f7df64db37f7407a70ecb050414db0
shiftgithub <mhturanmohammad@gmail.com>
2023-06-04 01:47:01 +0600

    updated project apps

---
+++ myadmin/serializers.py
@@ -0,0 +1,87 @@
+from rest_framework import serializers
+from myadmin.models import *
+from .models import *
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = '__all__'
+
+
+class DoctorsSerializer(serializers.ModelSerializer):
+    department = serializers.CharField(source='department_name')
+
+    class Meta:
+        model = Doctor
+        fields = ['id', 'name', 'description', 'department']
+
+
+class DoctorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Doctor
+        fields = '__all__'
+
+
+class BodyPartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BodyPart
+        fields = '__all__'
+
+
+class OrganSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Organ
+        fields = '__all__'
+
+
+class OrgansSerializer(serializers.ModelSerializer):
+    bodypart = serializers.CharField(source='bodypart_name')
+
+    class Meta:
+        model = Doctor
+        fields = ['id', 'name', 'description', 'bodypart']
+
+
+class OrganProblemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrgansProbleam
+        fields = '__all__'
+
+
+class OrganProblemsSerializer(serializers.ModelSerializer):
+    organ = serializers.CharField(source='organ_name')
+
+    class Meta:
+        model = OrgansProbleam
+        fields = ['id', 'name', 'description', 'organ']
+
+
+class ProblemSpecificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProblemSpecification
+        fields = '__all__'
+
+
+class ProblemSpecificationsSerializer(serializers.ModelSerializer):
+    organ = serializers.CharField(source='organ_name')
+
+    class Meta:
+        model = ProblemSpecification
+        fields = ['id', 'specification', 'description', 'organ']
+
+
+class DepartmentSpecificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DepartmentSpecification
+        fields = '__all__'
+
+
+class DepartmentSpecificationsSerializer(serializers.ModelSerializer):
+    department = serializers.CharField(source='department_name')
+    specification = serializers.CharField(source='problemspecification')
+
+    class Meta:
+        model = DepartmentSpecification
+        fields = ['id', 'description', 'department',
+                  'specification']
