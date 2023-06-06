0f039b9a4666ed7819a1c7ecd186c145c59fa41d
shiftgithub <mhturanmohammad@gmail.com>
2023-06-06 11:54:43 +0600

    upd : upload article image in static folder

--- myadmin/models.py
+++ myadmin/models.py
@@ -1,3 +1,5 @@
+import os
+import datetime
 from django.db import models
 
 
@@ -85,17 +87,32 @@ class FAQ(models.Model):
     def __str__(self):
         return self.question
 
+def article_filepath(request, filename):
+    # Get the original filename
+    old_filename = filename
+
+    # Get the current timestamp
+    time_now = datetime.datetime.now().strftime('%Y%m%d%H:%M:%S')
+
+    # Generate a new filename using the timestamp and original filename
+    filename = "%s_%s" % (time_now, old_filename)
+
+    # Return the relative path to the folder where you want to save the image
+    return os.path.join('static', 'uploads', 'article_images', filename)
+
 class Article(models.Model):
     heading = models.CharField(max_length=255)
     tag = models.CharField(max_length=255)
     description = models.CharField(max_length=1000)
-    image = models.ImageField(upload_to='images/article_images/')
+    image = models.ImageField(upload_to=article_filepath)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=False, null=True)
     deleted_at = models.DateTimeField(auto_now_add=False, null=True)
 
     def __str__(self):
         return self.heading
+
+
     
 
 
