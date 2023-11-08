7dce0b1123d1dbb3237bdf56f696ad9dde00d88f
shiftgithub <mhturanmohammad@gmail.com>
2023-11-08 15:53:41 +0600

    update : working on admin data store

--- admin/authentication/user/urls.py
+++ admin/authentication/user/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path("", protected_view(get_user_details), name="user_details"),
     path("add/", admin_form, name="admin_form"),
     path("store/", store_admin, name="store_admin"),
+    path("view/<int:admin_id>/", protected_view(store_admin), name="view_admin"),
+    path("update/<int:admin_id>/",protected_view(store_admin),name="edit_admin_form",),
+    path("edit/<int:admin_id>/", protected_view(store_admin), name="edit_admin"),
 ]
--- admin/authentication/user/webs.py
+++ admin/authentication/user/webs.py
@@ -6,20 +6,26 @@ from django.contrib import messages
 from backend.constants import *
 from admin.personal_data.views import *
 
+
 def get_user_details(request):
-    if request.session.get('user_role') == ROLE_DOCTOR:
-        user_id = request.session.get('user_id')
+    if request.session.get("user_role") == ROLE_ADMIN:
+        user_id = request.session.get("user_id")
+        admin_profile = AdminProfile.objects.filter(user_id=user_id).first()
+        admin_id = admin_profile.id
+        return redirect("view_admin", admin_id=admin_id)
+    elif request.session.get("user_role") == ROLE_DOCTOR:
+        user_id = request.session.get("user_id")
         doctor_profile = DoctorProfile.objects.filter(user_id=user_id).first()
         doctor_id = doctor_profile.id
-        return redirect('view_doctor_data', doctor_id=doctor_id)
-
-    elif request.session.get('user_role') == ROLE_PATIENT:
-        user_id = request.session.get('user_id')
+        return redirect("view_doctor_data", doctor_id=doctor_id)
+    elif request.session.get("user_role") == ROLE_PATIENT:
+        user_id = request.session.get("user_id")
         patient_profile = PatientProfile.objects.filter(user_id=user_id).first()
         patient_id = patient_profile.id
-        return redirect('view_patient', patient_id=patient_id)
+        return redirect("view_patient", patient_id=patient_id)
     else:
         pass
+
 
 def admin_form(request):
     response_gender = gender_list(request)
@@ -34,24 +40,22 @@ def admin_form(request):
     response_matrimony = matrimony_list(request)
     matrimony_data = response_matrimony.data
 
-    
     data = {
         "gender_data": gender_data,
         "religion_data": religion_data,
         "blood_group_data": blood_group_data,
         "matrimony_data": matrimony_data,
-        
     }
-    return render(request, 'authentication/user/templates/form.html',data)
+    return render(request, "authentication/user/templates/form.html", data)
 
 
 def store_admin(request):
     operation_response = store_admin_data(request)
-    if operation_response.data.get('status') == 200:
-        email = operation_response.data.get('email')
-        request.session['temp_verify_email'] = email
-        messages.add_message(request, messages.INFO, 'Please activate your account')
-        return redirect('otp_form')
+    if operation_response.data.get("status") == 200:
+        email = operation_response.data.get("email")
+        request.session["temp_verify_email"] = email
+        messages.add_message(request, messages.INFO, "Please activate your account")
+        return redirect("otp_form")
     else:
-        messages.add_message(request, messages.ERROR, 'Error in storing admin data')
-        return redirect('admin_form')
+        messages.add_message(request, messages.ERROR, "Error in storing admin data")
+        return redirect("admin_form")
