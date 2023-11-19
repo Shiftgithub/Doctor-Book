9d237e4e257aa8d2b9b73ce64f7b8a6241b9273c
shiftgithub <mhturanmohammad@gmail.com>
2023-11-20 02:10:02 +0600

    update : change some html code

--- landing/appointment/urls.py
+++ landing/appointment/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('date-time/<int:doctor_id>/', date_time_form, name='date_time_form'),
     path('get-date-time/', get_date_time, name='get_date_time'),
-    path('get-date-time/', generate_schedule_time, name='get_time'),
     path('appointment-schedule/form/', appointment_schedule_form, name='appointment_schedule_form'),
     path('appointment-schedule/', store_appointment_schedule, name='appointment_schedule'),
     path('appointment-schedule-method/', store_appointment_and_create_account, name='appointment_schedule_method'),
