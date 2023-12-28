777fb8cc79f7b4befba56ed7feeebd9f2208a073
shiftgithub <mhturanmohammad@gmail.com>
2023-12-29 00:42:19 +0600

    updated

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
     path('', include('admin.urls.urls')),
     path('', include('landing.url.urls')),
 ]
